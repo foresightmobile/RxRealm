439e70861e76627c7043a66b73498cacd5557237
Jack Walker <jack@foresightmobile.com>
2019-07-16 13:21:53 +0100

    Added library to package.swift file

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import Foundation
 
 let package = Package(
 	name: "RxRealm",
+    products: [
+        .library(name: "RxRealm", targets: ["Pod"])
+    ],
 	dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
         .package(url: "https://github.com/realm/realm-cocoa", .branch("tg/spm"))
