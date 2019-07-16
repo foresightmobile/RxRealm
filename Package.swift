d1cae62948b2a757efc0e94ad35e1895d6388667
Jack Walker <jack@foresightmobile.com>
2019-07-16 12:02:15 +0100

    Added realm dependency.

--- Package.swift
+++ Package.swift
@@ -9,8 +9,9 @@ let package = Package(
     ],
 	dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
+        .package(url: "https://github.com/realm/realm-cocoa", .branch("tg/spm"))
 	],
     targets: [
-        .target(name: "Pod", dependencies: ["RxSwift"], path: ".", sources: ["Pod"]),
+        .target(name: "Pod", dependencies: ["RxSwift", "realm-cocoa"], path: ".", sources: ["Pod"]),
     ]
 )
