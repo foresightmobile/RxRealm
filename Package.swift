5f145c7af7e91f6c5b8c6d8e5c6d26035eeab3b2
Jack Walker <jack@foresightmobile.com>
2019-07-16 11:10:07 +0100

    Added products and targets to package file.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,13 @@ import PackageDescription
 
 let package = Package(
 	name: "RxRealm",
+    products: [
+        .library(name: "RxRealm", targets: ["RxRealm"]),
+    ],
     dependencies: [
-            .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
+    ],
+    targets: [
+        .target(name: "RxRealm", dependencies: ["RxSwift"]),
     ]
 )
