d409b24ba19114aeef801c22a23e418625f589cc
Jack Walker <jack@foresightmobile.com>
2019-07-16 13:23:12 +0100

    no message

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import Foundation
 let package = Package(
 	name: "RxRealm",
     products: [
-        .library(name: "RxRealm", targets: ["Pod"])
+        .library(name: "Pod", targets: ["Pod"])
     ],
 	dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
