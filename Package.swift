3f8e2d51427a9345a7dfdbd6d0b7b976462726af
Jack Walker <jack@foresightmobile.com>
2019-07-16 11:24:51 +0100

    Updated Package.swift file.

--- Package.swift
+++ Package.swift
@@ -1,15 +1,13 @@
 // swift-tools-version:5.0
 import PackageDescription
+import Foundation
 
 let package = Package(
 	name: "RxRealm",
-    products: [
-        .library(name: "RxRealm", targets: ["RxRealm"]),
-    ],
-    dependencies: [
+	dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
-    ],
+	],
     targets: [
-        .target(name: "RxRealm", dependencies: ["RxSwift"]),
+        .target(name: "RxRealm", dependencies: ["RxSwift"], path: ".", sources: ["RxRealm"])
     ]
 )
