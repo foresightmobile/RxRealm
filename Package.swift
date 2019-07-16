ce0c012ce38880f6c927ccf56324f5bbc1e2ec66
Jack Walker <jack@foresightmobile.com>
2019-07-16 13:02:18 +0100

    Removed products section from package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,6 @@ import Foundation
 
 let package = Package(
 	name: "RxRealm",
-    products: [
-        .library(name: "Pod", targets: ["Pod"])
-    ],
 	dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .branch("master")),
         .package(url: "https://github.com/realm/realm-cocoa", .branch("tg/spm"))
